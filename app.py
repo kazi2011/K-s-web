09e146f6d416fb7b08bf33acce68c18697148520
kazi2011 <168892085+kazi2011@users.noreply.github.com>
2024-05-04 15:15:47 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+
+st.set_page_config(page_title="Kazi's",page_icon=":tada:",layout="wide")
+
+
+st.subheader("Hi, I am K.T.Huda :wave:")
+st.title("A coder from India")
+st.write("I read in class 7 in Rampurhat J.L.V and I am passionate about finding ways to use Python and java to be more efficient and be a software engineer.")
+st.write("My best friends are Ankit,Mayukh and Abhijit")
+st.write ("[learn any code>](https://www.youtube.com/@ApnaCollegeOfficial)")
